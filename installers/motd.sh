9ab3a2e7718af38bf1b056e2ec8489dfb52161d0
fsteltenkamp <florian@steltenkamp.de>
2019-01-12 23:09:36 +0100

    updated motd installler

--- installers/motd.sh
+++ installers/motd.sh
@@ -1,13 +1,14 @@
 function installmotd() {
     coloredEcho "Downloading Simple Motd..." cyan
-    downloadFile https://gitlab.com/snippets/1719592/raw /etc/motd
-    coloredEcho "Motd has been updated..." green
-    # collecting system info
-    hostname=$(uname -n)
-    kernver=$(uname -v)
-    kernres=$(uname -r)
-    cpuinfo=$(sudo lshw -short | grep processor)
-    cpuname=$()
+    downloadFile https://gitlab.com/snippets/1797332/raw /etc/motd
     # replace values
     coloredEcho "Replacing Detected Values..." cyan
+    sed 's/{HOSTNAME}/$HOSTNAME/g' /etc/motd
+    sed 's/{USER}/$CURUSER/g' /etc/motd
+    sed 's/{SYSADMINNAME}/$ADMINNAME/g' /etc/motd
+    sed 's/{SYSADMINMAIL}/$ADMINMAIL/g' /etc/motd
+    sed 's/{DISTRO}/$DISTRO/g' /etc/motd
+    sed 's/{KERN}/$KERN/g' /etc/motd
+    sed 's/{ARCH}/$ARCH/g' /etc/motd
+    coloredEcho "Motd has been updated..." green
 }
