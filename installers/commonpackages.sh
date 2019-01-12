16d1dbab8eb5be923aab1d42a4ae5106dae2aacf
fsteltenkamp <florian@steltenkamp.de>
2019-01-12 22:40:11 +0100

    this was stupid, already have a list in installer

--- installers/commonpackages.sh
+++
@@ -1,6 +0,0 @@
-function commonpackagesinstall() {
-    #get packages list
-    local packages="nload ethstatus mc ncdu htop"
-    coloredEcho "Installing Common Packages..." cyan
-    packageInstall packages
-}
