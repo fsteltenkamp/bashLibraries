a77ac1fc4a0bc3638205b4442311f50f27a029c6
fsteltenkamp <florian@steltenkamp.de>
2019-05-04 13:19:44 +0200

    added log() function

--- output.sh
+++ output.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 function coloredEcho(){
     local exp=$1;
     local color=$2;
@@ -17,4 +18,62 @@ function coloredEcho(){
     tput setaf $color;
     echo $exp;
     tput sgr0;
+}
+
+function log() {
+    TYPE=$1
+    MSG=$2
+    LOGFILENAME=$3
+
+    if [ -z $LOGFILENAME ]; then
+        LOGFILENAME="out.log"
+    fi
+
+    #colors
+    RED="\033[1;31m"
+    GREEN="\033[1;32m"
+    YELLOW="\033[1;33m"
+    BLUE="\033[0;34m"
+    MANGENTA="\033[0;35m"
+    CYAN="\033[1;36m"
+    GREY="\033[0;37m"
+    WHITE="\033[1;39m"
+    NOCOL="\033[00m"
+
+
+    TS=$(date "+$Y-$m-$d $T")
+
+    case $TYPE in
+        success)
+            echo -e "[${GREEN}SUCCESS${NOCOL}] $MSG"
+            echo "[$TS][SUCCESS] $MSG" >> $LOGFILENAME
+        ;;
+        info)
+            echo -e "[${CYAN}INFO${NOCOL}] $MSG"
+            echo "[$TS][INFO] $MSG" >> $LOGFILENAME
+        ;;
+        warn)
+            echo -e "[${YELLOW}WARN${NOCOL}] $MSG"
+            echo "[$TS][WARN] $MSG" >> $LOGFILENAME
+        ;;
+        error)
+            echo -e "[${RED}ERROR${NOCOL}] $MSG"
+            echo "[$TS][ERROR] $MSG" >> $LOGFILENAME
+        ;;
+        fatal)
+            echo -e "[${MAGENTA}FATAL${NOCOL}] $MSG"
+            echo "[$TS][FATAL] $MSG" >> $LOGFILENAME
+        ;;
+        debug)
+            echo -e "[${GREY}DEBUG${NOCOL}] $MSG"
+            echo "[$TS][DEBUG] $MSG" >> $LOGFILENAME
+        ;;
+        file)
+            echo "[$TS] $MSG" >> $LOGFILENAME
+        ;;
+        *)
+            echo -e "$MSG"
+            echo "[$TS] $MSG" >> $LOGFILENAME
+        ;;
+    esac
 }
