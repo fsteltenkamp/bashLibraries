8597b91c6d61ae78bae31f86d30ca323206f1de8
fsteltenkamp <florian@steltenkamp.de>
2019-05-10 12:00:17 +0200

    use replace function

--- installers/motd.sh
+++ installers/motd.sh
@@ -1,14 +1,15 @@
+#!/bin/bash
 function installmotd() {
     coloredEcho "Downloading Simple Motd..." cyan
     downloadFile https://gitlab.com/snippets/1797332/raw /etc/motd
     # replace values
     coloredEcho "Replacing Detected Values..." cyan
-    sed 's/{HOSTNAME}/$HOSTNAME/g' /etc/motd
-    sed 's/{USER}/$CURUSER/g' /etc/motd
-    sed 's/{SYSADMINNAME}/$ADMINNAME/g' /etc/motd
-    sed 's/{SYSADMINMAIL}/$ADMINMAIL/g' /etc/motd
-    sed 's/{DISTRO}/$DISTRO/g' /etc/motd
-    sed 's/{KERN}/$KERN/g' /etc/motd
-    sed 's/{ARCH}/$ARCH/g' /etc/motd
+    replace "{HOSTNAME}" "$HOSTNAME" "/etc/motd"
+    replace "{USER}" "$CURUSER" "/etc/motd"
+    replace "{SYSADMINNAME}" "$ADMINNAME" "/etc/motd"
+    replace "{SYSADMINMAIL}" "$ADMINMAIL" "/etc/motd"
+    replace "{DISTRO}" "$DISTRO" "/etc/motd"
+    replace "{KERN}" "$KERN" "/etc/motd"
+    replace "{ARCH}" "$ARCH" "/etc/motd"
     coloredEcho "Motd has been updated..." green
 }
