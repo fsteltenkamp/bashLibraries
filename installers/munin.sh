ec0964a68346df38404fa14510f0673cbedff098
fsteltenkamp <florian@steltenkamp.de>
2019-05-10 12:01:04 +0200

    plugin installation & shellcheck

--- installers/munin.sh
+++ installers/munin.sh
@@ -19,38 +19,41 @@ function muninNodeAddMaster() {
     #TODO: Get this working!
     coloredEcho "This feature is currently not working!" red
     return 0
-    MASTERADDRESS=$(user_input "Please Specify the IP-address of the Master-Node: ")
-    FORMATTED="allow ^"$(echo $MASTERADDRESS | sed -e 's/\./\\\./g')"$"
-    sed '/allow ^127\.0\.0\.1$/a $FORMATTED' /etc/munin/munin-node.conf
-    config=$(awk '1;/(allow \^127\\\.0\\\.0\\\.1\$)/{ print $FORMATTED; }' /etc/munin/munin-node.conf)
-    echo $config > test.txt
+    #MASTERADDRESS=$(user_input "Please Specify the IP-address of the Master-Node: ")
+    #FORMATTED="allow ^"$(echo $MASTERADDRESS | sed -e 's/\./\\\./g')"$"
+    #sed '/allow ^127\.0\.0\.1$/a $FORMATTED' /etc/munin/munin-node.conf
+    #config=$(awk '1;/(allow \^127\\\.0\\\.0\\\.1\$)/{ print $FORMATTED; }' /etc/munin/munin-node.conf)
+    #echo $config > test.txt
 }
 
 function muninPluginsInstall {
     coloredEcho "Installing Additional Plugins..." cyan
-    for plugin in $muninPlugins; do
-        pluginName=$plugin["name"]
-        pluginFile=$plugin["file"]
-        pluginUrl=$plugin["url"]
-        pluginAuthor=$plugin["author"]
+    COUNT=9 #adjust manually for now
+    for ((i=0; i<COUNT; i++))
+    do
+        #shellcheck disable=SC2154
+        pluginName=${muninPlugins[$i,1]}
+        pluginFile=${muninPlugins[$i,2]}
+        pluginUrl=${muninPlugins[$i,3]}
+        pluginAuthor=${muninPlugins[$i,4]}
 
         coloredEcho "Installing $pluginName Plugin by $pluginAuthor" cyan
-        downloadFile $pluginUrl "/usr/share/munin/plugins/$pluginFile"
-        chown root:root /usr/share/munin/plugins/$pluginFile
-        chmod 755 /usr/share/munin/plugins/$pluginFile
+        downloadFile "$pluginUrl" "/usr/share/munin/plugins/$pluginFile"
+        chown root:root "/usr/share/munin/plugins/$pluginFile"
+        chmod 755 "/usr/share/munin/plugins/$pluginFile"
     done
 }
 
 function muninPluginsConfigure() {
     coloredEcho "Configuring Plugins" yellow
-    PINGTARGETS=$(echo $(user_input "Specify Ping Targets (Separator: , ): ") | tr "," "\n")
+    PINGTARGETS=$( user_input "Specify Ping Targets (Separator: , ): " | tr "," "\n")
     for target in $PINGTARGETS; do
         coloredEcho "Setting $target up..." green
-        ln -s /usr/share/munin/plugins/ping_ /etc/munin/plugins/ping_$target
+        ln -s "/usr/share/munin/plugins/ping_" "/etc/munin/plugins/ping_$target"
     done
-    PORTLIST=$(echo $(user_input "Specify Ports to Scan (Separator: , ): ") | tr "," "\n")
+    PORTLIST=$( user_input "Specify Ports to Scan (Separator: , ): " | tr "," "\n")
     for port in $PORTLIST; do
         coloredEcho "Setting $port up..." green
-        ln -s /usr/share/munin/plugins/port_ /etc/munin/plugins/port_$port
+        ln -s "/usr/share/munin/plugins/port_" "/etc/munin/plugins/port_$port"
     done
 }
