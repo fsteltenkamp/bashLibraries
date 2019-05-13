6da1a404a5429142026328f0372748d0d1b7e22c
fsteltenkamp <florian@steltenkamp.de>
2019-05-13 14:55:40 +0200

    add library-checksum-testing WIP!

--- downloaders.sh
+++ downloaders.sh
@@ -1,19 +1,40 @@
 #!/bin/bash
+
 #getLibsfunction
 function getLib() {
     local libs="$*";
+    #check libs dir:
+    if [ ! -d "libs" ]; then
+        mkdir -p "libs";
+    fi
+    #download libs:
     for lib in $libs; do
-        libUrl=https://gitlab.com/fsteltenkamp/bashLibraries/raw/master/$lib.sh
+        libUrl="https://gitlab.com/fsteltenkamp/bashLibraries/raw/master/$lib.sh"
         libFile="libs/$lib.sh"
         if [ ! -f "$libFile" ]; then
             echo "Downloading Library $lib...";
             #download lib
             wget -qO "$libFile" "$libUrl"
         fi
+        echo "loading $lib Library..."
         # shellcheck disable=SC1090
         source "$libFile"
     done
 }
+
+#check libraries here:
+getLib "output" "hash" "various"
+log "info" "checking existing libraries..."
+updateLibs=$(hash_checkLibs)
+#todo change echo to getLibs if its working correctly
+echo "$updateLibs"
+
+#if there were any libraries to update, restart the script:
+#shellcheck disable=SC2236
+if [ ! -z "$updateLibs" ]; then
+    script_restart
+fi
+
 function getInstaller() {
     local installers="$*";
     #check for installers dir
