204c11b47109c537ff5877f707548678189263e7
fsteltenkamp <florian@steltenkamp.de>
2019-06-08 13:15:28 +0200

    add php packages from sury.org

--- installers/webserver.sh
+++ installers/webserver.sh
@@ -2,4 +2,10 @@
 function webserverInstall() {
     coloredEcho "Installing Webserver Packages" cyan
     packageInstall apache2 php php-curl php-mysql
+    if prompt_confirm "Install newest PHP Releases?"; then
+        packageInstall apt-transport-https lsb-release ca-certificates
+        wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+        sh -c 'echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
+        packageUpdate
+    fi
 }
